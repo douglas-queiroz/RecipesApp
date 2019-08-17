2e2dade96e2cbc116d8dc12f7674742ed4c218f9
Douglas Queiroz <douglasq16@gmail.com>
2019-08-17 22:43:10 +0100

    Create welcome screen

---
+++ app/src/main/java/com/douglas/recipesapp/module/welcome/WelcomeActivity.kt
@@ -0,0 +1,13 @@
+package com.douglas.recipesapp.module.welcome
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.douglas.recipesapp.R
+
+class WelcomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+    }
+}
