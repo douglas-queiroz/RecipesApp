07a8437976bb287819a194e98c67a1bbac32f6ce
Douglas Queiroz <douglasq16@gmail.com>
2019-08-17 20:12:20 +0100

    Create SessionUtil

---
+++ app/src/main/java/com/douglas/recipesapp/util/SessionUtilImpl.kt
@@ -0,0 +1,8 @@
+package com.douglas.recipesapp.util
+
+class SessionUtilImpl: SessionUtil {
+
+    override fun getApiKey(): String {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
